890d90892917a202ca068df78db18199122a9e49
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-26 16:19:59 +0800

    feat: Add user sign up and sign in template for future implementation

---
+++ src/api/controllers/userController.ts
@@ -0,0 +1,13 @@
+export const SignUpUser = (req: Request, res: Response) => {
+  try {
+  } catch (err: any) {
+    console.error(err);
+  }
+};
+
+export const SignInUser = (req: Request, res: Response) => {
+  try {
+  } catch (err: any) {
+    console.error(err);
+  }
+};
