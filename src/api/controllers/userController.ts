4e4106d45f1ec18b1428c360f792103d26e3df1f
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-27 06:10:56 +0800

    refactor userController.ts to comment inital user code

--- src/api/controllers/userController.ts
+++ src/api/controllers/userController.ts
@@ -1,24 +1,15 @@
-import { Request, Response } from "express";
-import userServices from "../services/userServices";
+// import { Request, Response } from "express";
+// import userServices from "../services/userServices";
 
-async function createUser(req: Request, res: Response) {
-  const { email, password } = req.body;
-  try {
-    const user = await userServices.CreateUser(email, password);
-    console.log("User created successfully!");
-    res.status(201).json(user);
-  } catch (err: any) {
-    res.status(500).json({ error: err });
-  }
-}
+// async function createUser(req: Request, res: Response) {
+//   const { email, password } = req.body;
+//   try {
+//     const user = await userServices.CreateUser(email, password);
+//     console.log("User created successfully!");
+//     res.status(201).json(user);
+//   } catch (err: any) {
+//     res.status(500).json({ error: err });
+//   }
+// }
 
-async function getAllUsers(req: Request, res: Response) {
-  try {
-    const users = await userServices.FindAllUsers();
-    res.status(200).json(users);
-  } catch (err: any) {
-    res.status(500).json({ error: err });
-  }
-}
-
-export default { createUser, getAllUsers };
+// export default { createUser };
