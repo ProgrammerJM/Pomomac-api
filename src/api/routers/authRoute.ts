cacc3e667a2a1b6e620a715657d88f46b18a0dd0
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-27 06:09:58 +0800

    feat: Add authentication routes for signup, login, token refresh, and authentication check

---
+++ src/api/routers/authRoute.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import authController from "../controllers/authController";
+
+const router: Router = Router();
+
+router.post("/signup", authController.authenticateSignUpUser);
+router.post("/login", authController.authenticateLoginUser);
+router.post("/refreshToken", authController.refreshToken);
+router.post("/auth", authController.checkAuth);
+
+export default router;
