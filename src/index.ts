94a909b0c688234d6f4e02bafb2fe53bebbbbeef
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-28 03:27:45 +0800

    refactor: Update index.ts to enable CORS and update route paths

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
 import express, { Application, Request, Response, NextFunction } from "express";
-// import userRoutes from "./api/routers/userRoutes";
 import authRoute from "./api/routers/authRoute";
-import users from "./api/routers/userRoutes";
+import userRoutes from "./api/routers/userRoutes";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 require("dotenv").config();
 
-// Configure CORS
 const corsOptions = {
-  origin: "http://localhost:3000", // Allow requests from this origin
-  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
 };
 
 const app: Application = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 
-// app.use("/api", userRoutes);
-app.use("/api", authRoute);
-app.use("/users", users);
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoutes);
 
 app.listen(5000, () => {
   console.log("Server is listening to port 5000...");
