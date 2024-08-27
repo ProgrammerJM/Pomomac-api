d1c5f03a672b807e7c341db9b49984636eead2b7
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-28 03:31:18 +0800

    refactor: Update userRoutes to use userController for profile route

--- src/api/routers/userRoutes.ts
+++ src/api/routers/userRoutes.ts
@@ -1,38 +1,9 @@
-import { Request, Response, NextFunction } from "express";
 import express from "express";
-import { isAuthenticated } from "../middleware/auth";
-import { findUserById } from "../services/userServices";
-
-// Define a custom request interface that extends the default Request
-interface CustomRequest extends Request {
-  payload?: { userId: string }; // Optional payload
-}
+import { isAuthenticated } from "../middleware/authMiddleware";
+import userController from "../controllers/userController";
 
 const router = express.Router();
 
-router.get(
-  "/profile",
-  isAuthenticated,
-  async (req: CustomRequest, res: Response, next: NextFunction) => {
-    try {
-      if (!req.payload) {
-        return res.status(401).json({ message: "Unauthorized from useRoutes" });
-      }
-
-      const { userId } = req.payload;
-      const user = await findUserById(userId);
-
-      if (!user) {
-        return res.status(404).json({ message: "User not found" });
-      }
-
-      // Ensure sensitive information such as password is not sent in the response
-      const { password, ...userWithoutPassword } = user;
-      res.json(userWithoutPassword);
-    } catch (err) {
-      next(err);
-    }
-  }
-);
+router.get("/profile", isAuthenticated, userController.getProfile);
 
 export default router;
