ccd66d60603c59b55d4eb2f000f2e79daa3e2f4e
John Mark Tizado <johnmarktizado@gmail.com>
2024-09-02 03:12:31 +0800

    refactor: Update taskRoutes to use isAuthenticated middleware for todos routes

--- src/api/routers/taskRoutes.ts
+++ src/api/routers/taskRoutes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import taskController from "../controllers/taskController";
+import isAuthenticated from "../middleware/authMiddleware";
 
 const router: Router = Router();
 
-router.get("/tasks/:userId", taskController.getTask);
-router.post("/tasks", taskController.createTask);
-router.put("/tasks/:id", taskController.updateTask);
-router.delete("/tasks/:id", taskController.deleteTask);
+router.get("/todos", isAuthenticated, taskController.getTask);
+router.post("/todo", isAuthenticated, taskController.createTask);
+
+// router.get("/tasks/:userId", taskController.getTask);
+// router.post("/tasks", taskController.createTask);
+// router.put("/tasks/:id", taskController.updateTask);
+// router.delete("/tasks/:id", taskController.deleteTask);
 
 export default router;
