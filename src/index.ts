391fad29a650d52a094a3e46af97c081f08152aa
John Mark Tizado <johnmarktizado@gmail.com>
2024-09-02 03:10:41 +0800

    refactor: Update import paths for interval, session, and task routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import express, { Application } from "express";
 import authRoute from "./api/routers/authRoute";
 import userRoutes from "./api/routers/userRoutes";
-import taskService from "./api/routers/taskRoutes";
-import sessionService from "./api/routers/sessionRoutes";
-import intervalService from "./api/routers/intervalRoutes";
+import taskRoutes from "./api/routers/taskRoutes";
+import sessionRoutes from "./api/routers/sessionRoutes";
+import intervalRoutes from "./api/routers/intervalRoutes";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import "dotenv/config";
@@ -21,9 +21,9 @@ app.use(cors(corsOptions));
 
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoutes);
-app.use("/api/task", taskService);
-app.use("/api/session", sessionService);
-app.use("/api/interval", intervalService);
+app.use("/api/task", taskRoutes);
+app.use("/api/session", sessionRoutes);
+app.use("/api/interval", intervalRoutes);
 
 app.listen(5000, () => {
   console.log("Server is listening to port 5000...");
