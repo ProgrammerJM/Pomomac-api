3ae71497141692ba2fe0579c8e4b10933bff93e9
John Mark Tizado <johnmarktizado@gmail.com>
2024-09-12 22:57:56 +0800

    Refactor taskRoutes to add deleteTask route with isAuthenticated middleware

--- src/api/routers/taskRoutes.ts
+++ src/api/routers/taskRoutes.ts
@@ -6,6 +6,7 @@ const router: Router = Router();
 
 router.get("/todos", isAuthenticated, taskController.getTask);
 router.post("/todo", isAuthenticated, taskController.createTask);
+router.delete("/todo/:id", isAuthenticated, taskController.deleteTask);
 
 // router.get("/tasks/:userId", taskController.getTask);
 // router.post("/tasks", taskController.createTask);
